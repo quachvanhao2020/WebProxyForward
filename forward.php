6368961701c4754141d6b1029528a2835de439b7
hao <quachvanhao2020@gmail.com>
2024-01-07 12:11:18 +0700

    f

---
+++ forward.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    "ukfake1.com" => "https://uk88.top",
+    "ukfake1.com" => "https://u.pcloud.link/publink/show?code=XZCNyX0ZoRoinekg4IXKraPzHUC1052RoSly"
+];
--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
-require_once __DIR__."/vendor/autoload.php";
 use Proxy\Proxy;
 use Proxy\Adapter\Guzzle\GuzzleAdapter;
 use Proxy\Filter\RemoveEncodingFilter;
 use Laminas\Diactoros\ServerRequestFactory;
+require_once __DIR__."/vendor/autoload.php";
+$forward = require_once __DIR__."/forward.php";
 $request = ServerRequestFactory::fromGlobals();
 $guzzle = new GuzzleHttp\Client();
 $proxy = new Proxy(new GuzzleAdapter($guzzle));
 $proxy->filter(new RemoveEncodingFilter());
-$path = $request->getUri()->getPath();
+$uri = $request->getUri();
+$path = $uri->getPath();
+$host = $uri->getHost();
+$to = $forward[$host];
 try {
-    $response = $proxy->forward($request)->to('https://uk88.top/');
+    $response = $proxy->forward($request)->to($to);
     ob_start();
     (new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
     $output = ob_get_contents(); 
     ob_end_clean();
-    if($path == "/"){
-        //$output = str_replace("UK88","ACB888",$output);
-    }
     echo $output;
+    return;
     if($path == "/"){
         echo '<script src="/inject.js"></script>';
         echo '<link rel="stylesheet" type="text/css" href="/css.css">';
